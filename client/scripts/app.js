ac6b7c1c79690550497a616e9fe344a5decd3d00
moriah-rahamim <moriahfaye@gmail.com>
2018-01-26 15:13:55 -0800

    implemented post request for app.send method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,20 @@
 // YOUR CODE HERE:
+var app = {
+  init: function() {},
+  send: function(message) {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    }); 
+  }
+};
